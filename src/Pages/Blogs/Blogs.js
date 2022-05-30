b0f8f956bf9482d6c7eff8ad128c1c6d0e7c36c8
adritadeb <adritad2019@gmail.com>
2022-05-30 18:35:27 +0600

    third qna done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -13,6 +13,10 @@ const Blogs = () => {
                     <h2 className='text-2xl'>2. What are the different ways to manage a state in a React application?</h2>
                     <p className='ml-2 my-5'>-- There are various ways to manage a state in a react application. We can manage the local state by using the useState hook. To manage the global state, we can use multiple components. By lifting state up, we can manage a global state. We can also use useReducer to manage local and global state. To manage a server state, we can use react query or many other tools.</p>
                 </div>
+                <div>
+                    <h2 className='text-2xl'>3. How does prototypical inheritance work?</h2>
+                    <p className='ml-2 my-5'>-- The prototypical inheritance is a javascript feature to add methods and properties to objects. By using it, an object can share, extend or copy properties and methods of another. When an object tries to access a property or method of another one. At first, javascript searches it on the object itself. If it is not there, javascript searches it to the object's prototype. If still no match is found, then javascript will search the linked object's prototype and continue it till reached the end of the prototype. The object inherits the methods and properties of another.</p>
+                </div>
             </div>
         </div>
     );
